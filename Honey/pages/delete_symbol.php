3f47d19c49244373ae2b8b1cacd103e99bab5cd7
yanelp <yanela.carllinni@gmail.com>
2013-08-05 04:50:25 -0700

    Baja logica de simbolos

---
+++ Honey/pages/delete_symbol.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once('functions.php');
+require_once( 'file_api.php' );
+
+
+html_page_top( plugin_lang_get( 'title' ) );
+auth_ensure_user_authenticated();
+
+print_cu_menu();
+
+EVENT_LAYOUT_RESOURCES;
+
+//simbolo a borrar
+$id_symbol = gpc_get_int( 'id_symbol' );
+
+//borramos logicamente el simbolo
+$t_repo_table = plugin_table( 'symbol', 'honey' );
+$t_query_usecase = 'UPDATE '.$t_repo_table.' set active=1
+					where id= ' . db_param() . '';
+
+$g_result_delete_usecase=db_query_bound( $t_query_usecase, array( $id_symbol) );
+
+
+echo "<p>Deleted data</p>";
+
+$t_page=plugin_page('view_symbols_page');
+echo '<br><br>';
+echo "<a href=\"$t_page\">Back</a>";
+
+echo "<br>";
+
+$t_url= plugin_page( 'view_symbols_page' );
+
+html_page_bottom( );
+
+html_meta_redirect_honey( $t_url, $p_time = null);
+
+
+?>
---
+++ Honey/pages/delete_symbol_page.php
@@ -0,0 +1,142 @@
+<?php
+
+require_once('functions.php');
+require_once( 'file_api.php' );
+
+
+html_page_top( plugin_lang_get( 'title' ) );
+auth_ensure_user_authenticated();
+
+print_lel_menu();
+
+EVENT_LAYOUT_RESOURCES;
+
+
+//symbol a borrar
+$id_symbol =$_REQUEST['id_symbol_hidden'];
+$type_symbol =$_REQUEST['type_symbol_hidden'];
+
+
+
+if ($type_symbol == 1){
+
+		$t_repo_table= plugin_table( 'actor', 'honey' );
+
+										$query_search = 'SELECT *
+														  FROM '.$t_repo_table.' 
+														  WHERE id_symbol=' . db_param().
+														  ' AND active=0';
+
+		$result_search = db_query_bound( $query_search, array($id_symbol) );
+
+		$count_actors = db_num_rows( $result_search );
+
+		if($count_actors > 0){
+		
+		?>
+			<div align="center">
+			<table class="width90">
+			<tr class="row-category">
+			<td>The Symbol <?php echo "<a  href=\"$t_page_return\">".str_pad($id_symbol, 7, "0", STR_PAD_LEFT)."</a>";?> is being referenced  by the following actors:</td>
+			</tr>
+			<?php
+			while($row_search = db_fetch_array( $result_search )){
+				 $id_actor = $row_search['id'];
+				 $actor_name = $row_search['name'];
+				 $t_page_go =  plugin_page( 'update_actor_page' );
+				 $t_page_go=$t_page_go."&id_actor=".$id_actor;
+				 ?>
+				<tr class="row-category" <?php echo helper_alternate_class() ?> >
+				<td class="form-title" colspan="2"> 
+									<?php echo "<a  href=\"$t_page_go\">".str_pad($id_actor, 7, "0", STR_PAD_LEFT)."</a>";?>-<?php echo $actor_name;?>
+				</td>
+
+				</tr>
+			<?php
+
+		
+		}//fin while
+
+	}//fin if ($count_actors)
+
+   } //fin if ($type_symbol == 4)
+    
+
+    if ($type_symbol == 4){
+
+		$t_repo_table= plugin_table( 'usecase', 'honey' );
+
+										$query_search = 'SELECT *
+														  FROM '.$t_repo_table.' 
+														  WHERE id_symbol=' . db_param().
+														  ' AND active=0';
+
+		$result_search = db_query_bound( $query_search, array($id_symbol) );
+
+		$count_cu = db_num_rows( $result_search );
+
+		if($count_cu > 0){
+			
+			?>
+				<div align="center">
+				<table class="width90">
+				<tr class="row-category">
+				<td>The Symbol <?php echo "<a  href=\"$t_page_return\">".str_pad($id_symbol, 7, "0", STR_PAD_LEFT)."</a>";?> is being referenced  by the following use cases:</td>
+				</tr>
+				<?php
+				while($row_search = db_fetch_array( $result_search )){
+					 $id_usecase = $row_search['id'];
+					 $uc_name = $row_search['name'];
+					 $t_page_go =  plugin_page( 'usecase_page' );
+					 $t_page_go=$t_page_go."&id_usecase=".$id_usecase;
+					 ?>
+					<tr class="row-category" <?php echo helper_alternate_class() ?> >
+					<td class="form-title" colspan="2"> 
+										<?php echo "<a  href=\"$t_page_go\">".str_pad($id_usecase, 7, "0", STR_PAD_LEFT)."</a>";?>-<?php echo $uc_name;?>
+					</td>
+
+					</tr>
+				<?php
+
+			
+			}//fin while
+
+		}//fin if ($count_cu)
+
+	
+	}//fin ($type_symbol == 4)
+
+$t_page=plugin_page( 'delete_symbol' );
+$t_page=$t_page."&id_symbol=".$id_symbol;
+
+?>
+
+
+
+
+<div align="center">
+<form id="form1" action="<?php echo plugin_page( $t_page ); ?>" method="POST" enctype="multipart/form-data">
+
+
+<table class="width90">
+
+<tr class="row-category" <?php echo helper_alternate_class() ?>>
+		<td colspan="2">Are you sure you wish to delete these use Symbol</td>
+</tr>
+<tr class="row-category">
+		<td class="form-title" colspan="2">
+		<input type="button" value="Delete Symbol" onClick="javascript:go_page(null, null ,'<?php echo $t_page;?>')"/></td>
+</tr>
+</table>
+</form>
+
+
+</div>
+
+
+
+<?php
+
+html_page_bottom1( );
+
+?>
